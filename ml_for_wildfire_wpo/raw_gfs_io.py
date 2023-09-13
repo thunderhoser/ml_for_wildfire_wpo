689102d756c608933b29ae62654e536628ec6af2
thunderhoser <lagerqui@ualberta.ca>
2023-09-13 12:55:21 -0600

    FOO

--- ml_for_wildfire_wpo/raw_gfs_io.py
+++ ml_for_wildfire_wpo/raw_gfs_io.py
@@ -293,7 +293,7 @@ def read_file(grib2_file_name, desired_row_indices, desired_column_indices,
 
             this_data_matrix = this_data_matrix[desired_row_indices, :]
             this_data_matrix = this_data_matrix[:, desired_column_indices]
-            assert not numpy.any(numpy.isnan(this_data_matrix))
+            # assert not numpy.any(numpy.isnan(this_data_matrix))
 
             data_matrix_3d[..., p, f] = (
                 this_data_matrix * FIELD_NAME_TO_CONV_FACTOR[field_names_3d[f]]
@@ -325,7 +325,7 @@ def read_file(grib2_file_name, desired_row_indices, desired_column_indices,
 
         this_data_matrix = this_data_matrix[desired_row_indices, :]
         this_data_matrix = this_data_matrix[:, desired_column_indices]
-        assert not numpy.any(numpy.isnan(this_data_matrix))
+        # assert not numpy.any(numpy.isnan(this_data_matrix))
 
         data_matrix_2d[..., f] = (
             this_data_matrix * FIELD_NAME_TO_CONV_FACTOR[field_names_2d[f]]
