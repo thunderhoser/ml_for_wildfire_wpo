13d5129496bbcbba8bbfcef9ee9690b2ae0fb5c1
thunderhoser <lagerqui@ualberta.ca>
2023-09-21 14:12:17 -0600

    Added a bunch of helper methods to get towards building a full generator.

--- ml_for_wildfire_wpo/utils/canadian_fwi_utils.py
+++ ml_for_wildfire_wpo/utils/canadian_fwi_utils.py
@@ -1,5 +1,6 @@
 """Helper methods for Canadian fire-weather indices."""
 
+import numpy
 from gewittergefahr.gg_utils import error_checking
 
 LATITUDE_DIM = 'latitude_deg_n'
@@ -38,3 +39,60 @@ def check_field_name(field_name):
     )
 
     raise ValueError(error_string)
+
+
+def get_field(fwi_table_xarray, field_name):
+    """Extracts one field from xarray table.
+
+    M = number of rows in grid
+    N = number of columns in grid
+
+    :param fwi_table_xarray: xarray table with Canadian FWI variables.
+    :param field_name: Field name.
+    :return: data_matrix: M-by-N numpy array of data values.
+    """
+
+    error_checking.assert_is_string(field_name)
+
+    k = numpy.where(
+        fwi_table_xarray.coords[FIELD_DIM].values == field_name
+    )[0][0]
+
+    return fwi_table_xarray[DATA_KEY].values[..., k]
+
+
+def subset_by_row(fwi_table_xarray, desired_row_indices):
+    """Subsets xarray table by grid row.
+
+    :param fwi_table_xarray: xarray table with Canadian FWI variables.
+    :param desired_row_indices: 1-D numpy array with indices of desired rows.
+    :return: fwi_table_xarray: Same as input but maybe with fewer
+        rows.
+    """
+
+    error_checking.assert_is_numpy_array(desired_row_indices, num_dimensions=1)
+    error_checking.assert_is_integer_numpy_array(desired_row_indices)
+    error_checking.assert_is_geq_numpy_array(desired_row_indices, 0)
+
+    return fwi_table_xarray.isel({LATITUDE_DIM: desired_row_indices})
+
+
+def subset_by_column(fwi_table_xarray, desired_column_indices):
+    """Subsets xarray table by grid column.
+
+    :param fwi_table_xarray: xarray table with Canadian FWI variables.
+    :param desired_column_indices: 1-D numpy array with indices of desired
+        columns.
+    :return: fwi_table_xarray: Same as input but maybe with fewer
+        columns.
+    """
+
+    error_checking.assert_is_numpy_array(
+        desired_column_indices, num_dimensions=1
+    )
+    error_checking.assert_is_integer_numpy_array(desired_column_indices)
+    error_checking.assert_is_geq_numpy_array(desired_column_indices, 0)
+
+    return fwi_table_xarray.isel(
+        {LONGITUDE_DIM: desired_column_indices}
+    )
