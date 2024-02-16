f2cf168509d4ed98acfa767fe315df87a7a4eb29
thunderhoser <lagerqui@ualberta.ca>
2024-02-15 17:13:57 -0700

    FOO

--- ml_for_wildfire_wpo/plot_generator_outputs.py
+++ ml_for_wildfire_wpo/plot_generator_outputs.py
@@ -17,7 +17,6 @@ import longitude_conversion as lng_conversion
 import file_system_utils
 import border_io
 import gfs_utils
-import canadian_fwi_utils
 import neural_net
 import plotting_utils
 import gfs_plotting
@@ -41,18 +40,20 @@ def _run(template_file_name, output_dir_name,
          gfs_predictor_field_names, gfs_pressure_levels_mb,
          gfs_predictor_lead_times_hours, gfs_normalization_file_name,
          era5_constant_file_name, era5_constant_predictor_field_names,
-         target_field_name, target_lead_time_days, target_lag_times_days,
-         target_cutoffs_for_classifn, target_normalization_file_name,
+         target_field_names, target_lead_time_days, target_lag_times_days,
+         gfs_forecast_target_lead_times_days, target_normalization_file_name,
          num_examples_per_batch, sentinel_value,
          gfs_dir_name_for_training, target_dir_name_for_training,
+         gfs_forecast_target_dir_name_for_training,
          init_date_limit_strings_for_training,
          gfs_dir_name_for_validation, target_dir_name_for_validation,
+         gfs_forecast_target_dir_name_for_validation,
          init_date_limit_strings_for_validation,
          num_epochs, num_training_batches_per_epoch,
          num_validation_batches_per_epoch,
          plateau_patience_epochs, plateau_learning_rate_multiplier,
          early_stopping_patience_epochs):
-    """Plots generator outputs.
+    """Trains neural net.
 
     This is effectively the main method.
 
@@ -68,18 +69,20 @@ def _run(template_file_name, output_dir_name,
     :param gfs_normalization_file_name: Same.
     :param era5_constant_file_name: Same.
     :param era5_constant_predictor_field_names: Same.
-    :param target_field_name: Same.
+    :param target_field_names: Same.
     :param target_lead_time_days: Same.
     :param target_lag_times_days: Same.
-    :param target_cutoffs_for_classifn: Same.
+    :param gfs_forecast_target_lead_times_days: Same.
     :param target_normalization_file_name: Same.
     :param num_examples_per_batch: Same.
     :param sentinel_value: Same.
     :param gfs_dir_name_for_training: Same.
     :param target_dir_name_for_training: Same.
+    :param gfs_forecast_target_dir_name_for_training: Same.
     :param init_date_limit_strings_for_training: Same.
     :param gfs_dir_name_for_validation: Same.
     :param target_dir_name_for_validation: Same.
+    :param gfs_forecast_target_dir_name_for_validation: Same.
     :param init_date_limit_strings_for_validation: Same.
     :param num_epochs: Same.
     :param num_training_batches_per_epoch: Same.
@@ -110,10 +113,20 @@ def _run(template_file_name, output_dir_name,
         era5_constant_predictor_field_names = None
 
     if (
-            len(target_cutoffs_for_classifn) == 1 and
-            target_cutoffs_for_classifn[0] <= 0
+            len(gfs_forecast_target_lead_times_days) == 1 and
+            gfs_forecast_target_lead_times_days[0] <= 0
     ):
-        target_cutoffs_for_classifn = None
+        gfs_forecast_target_lead_times_days = None
+        gfs_forecast_target_dir_name_for_training = None
+        gfs_forecast_target_dir_name_for_validation = None
+
+    if (
+            gfs_forecast_target_dir_name_for_training is None or
+            gfs_forecast_target_dir_name_for_validation is None
+    ):
+        gfs_forecast_target_lead_times_days = None
+        gfs_forecast_target_dir_name_for_training = None
+        gfs_forecast_target_dir_name_for_validation = None
 
     training_option_dict = {
         neural_net.INNER_LATITUDE_LIMITS_KEY: inner_latitude_limits_deg_n,
@@ -127,16 +140,19 @@ def _run(template_file_name, output_dir_name,
         neural_net.ERA5_CONSTANT_PREDICTOR_FIELDS_KEY:
             era5_constant_predictor_field_names,
         neural_net.ERA5_CONSTANT_FILE_KEY: era5_constant_file_name,
-        neural_net.TARGET_FIELD_KEY: target_field_name,
+        neural_net.TARGET_FIELDS_KEY: target_field_names,
         neural_net.TARGET_LEAD_TIME_KEY: target_lead_time_days,
         neural_net.TARGET_LAG_TIMES_KEY: target_lag_times_days,
-        neural_net.TARGET_CUTOFFS_KEY: target_cutoffs_for_classifn,
+        neural_net.GFS_FCST_TARGET_LEAD_TIMES_KEY:
+            gfs_forecast_target_lead_times_days,
         neural_net.TARGET_NORM_FILE_KEY: target_normalization_file_name,
         neural_net.BATCH_SIZE_KEY: num_examples_per_batch,
         neural_net.SENTINEL_VALUE_KEY: sentinel_value,
         neural_net.INIT_DATE_LIMITS_KEY: init_date_limit_strings_for_training,
         neural_net.GFS_DIRECTORY_KEY: gfs_dir_name_for_training,
-        neural_net.TARGET_DIRECTORY_KEY: target_dir_name_for_training
+        neural_net.TARGET_DIRECTORY_KEY: target_dir_name_for_training,
+        neural_net.GFS_FORECAST_TARGET_DIR_KEY:
+            gfs_forecast_target_dir_name_for_training
     }
 
     gfs_2d_field_names = [
@@ -153,6 +169,10 @@ def _run(template_file_name, output_dir_name,
     num_3d_fields = len(gfs_3d_field_names)
     num_2d_fields = len(gfs_2d_field_names)
 
+    num_target_fields = len(target_field_names)
+    num_target_lag_times = len(target_lag_times_days)
+    num_gfs_forecast_target_lead_times = len(gfs_forecast_target_lead_times_days)
+
     generator_object = neural_net.data_generator(training_option_dict)
     example_index = -1
 
@@ -165,7 +185,7 @@ def _run(template_file_name, output_dir_name,
         predictor_matrices, target_matrix_with_weights = next(generator_object)
         gfs_3d_predictor_matrix = predictor_matrices[0]
         gfs_2d_predictor_matrix = predictor_matrices[1]
-        # lagged_target_predictor_matrix = predictor_matrices[2]
+        laglead_target_predictor_matrix = predictor_matrices[2]
 
         this_num_examples = target_matrix_with_weights.shape[0]
 
@@ -323,62 +343,213 @@ def _run(template_file_name, output_dir_name,
                     )
                     pyplot.close(figure_object)
 
-            figure_object, axes_object = pyplot.subplots(
-                1, 1,
-                figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
-            )
+            for j in range(num_target_fields):
+                for l in range(num_target_lag_times):
+                    min_colour_value = numpy.percentile(
+                        laglead_target_predictor_matrix[i, ..., l, j], 1.
+                    )
+                    max_colour_value = numpy.percentile(
+                        laglead_target_predictor_matrix[i, ..., l, j], 99.
+                    )
 
-            colour_map_object, colour_norm_object = (
-                fwi_plotting.field_to_colour_scheme(
-                    canadian_fwi_utils.FFMC_NAME
+                    figure_object, axes_object = pyplot.subplots(
+                        1, 1,
+                        figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+                    )
+                    gfs_plotting.plot_field(
+                        data_matrix=laglead_target_predictor_matrix[i, ..., l, j],
+                        grid_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                        grid_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                        colour_map_object=pyplot.get_cmap('viridis'),
+                        colour_norm_object=pyplot.Normalize(
+                            vmin=min_colour_value, vmax=max_colour_value
+                        ),
+                        axes_object=axes_object,
+                        plot_colour_bar=True
+                    )
+
+                    plotting_utils.plot_borders(
+                        border_latitudes_deg_n=border_latitudes_deg_n,
+                        border_longitudes_deg_e=border_longitudes_deg_e,
+                        axes_object=axes_object,
+                        line_colour=numpy.full(3, 0.)
+                    )
+                    plotting_utils.plot_grid_lines(
+                        plot_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                        plot_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                        axes_object=axes_object,
+                        meridian_spacing_deg=20.,
+                        parallel_spacing_deg=10.
+                    )
+
+                    axes_object.set_xlim(
+                        numpy.min(OUTER_GRID_LONGITUDES_DEG_E),
+                        numpy.max(OUTER_GRID_LONGITUDES_DEG_E)
+                    )
+                    axes_object.set_ylim(
+                        numpy.min(OUTER_GRID_LATITUDES_DEG_N),
+                        numpy.max(OUTER_GRID_LATITUDES_DEG_N)
+                    )
+
+                    title_string = 'Actual {0:s} at {1:d}-day lag'.format(
+                        target_field_names[j],
+                        target_lag_times_days[l]
+                    )
+                    axes_object.set_title(title_string)
+
+                    output_file_name = (
+                        '{0:s}/example{1:03d}_{2:s}_minus{3:03d}days.jpg'
+                    ).format(
+                        output_dir_name,
+                        example_index,
+                        target_field_names[j].replace('_', '-'),
+                        target_lag_times_days[l]
+                    )
+
+                    print('Saving figure to: "{0:s}"...'.format(
+                        output_file_name
+                    ))
+                    figure_object.savefig(
+                        output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+                        pad_inches=0, bbox_inches='tight'
+                    )
+                    pyplot.close(figure_object)
+
+            for j in range(num_target_fields):
+                for this_l in range(num_gfs_forecast_target_lead_times):
+                    l = this_l + num_target_lag_times
+
+                    min_colour_value = numpy.percentile(
+                        laglead_target_predictor_matrix[i, ..., l, j], 1.
+                    )
+                    max_colour_value = numpy.percentile(
+                        laglead_target_predictor_matrix[i, ..., l, j], 99.
+                    )
+
+                    figure_object, axes_object = pyplot.subplots(
+                        1, 1,
+                        figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
+                    )
+                    gfs_plotting.plot_field(
+                        data_matrix=laglead_target_predictor_matrix[i, ..., l, j],
+                        grid_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                        grid_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                        colour_map_object=pyplot.get_cmap('viridis'),
+                        colour_norm_object=pyplot.Normalize(
+                            vmin=min_colour_value, vmax=max_colour_value
+                        ),
+                        axes_object=axes_object,
+                        plot_colour_bar=True
+                    )
+
+                    plotting_utils.plot_borders(
+                        border_latitudes_deg_n=border_latitudes_deg_n,
+                        border_longitudes_deg_e=border_longitudes_deg_e,
+                        axes_object=axes_object,
+                        line_colour=numpy.full(3, 0.)
+                    )
+                    plotting_utils.plot_grid_lines(
+                        plot_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                        plot_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                        axes_object=axes_object,
+                        meridian_spacing_deg=20.,
+                        parallel_spacing_deg=10.
+                    )
+
+                    axes_object.set_xlim(
+                        numpy.min(OUTER_GRID_LONGITUDES_DEG_E),
+                        numpy.max(OUTER_GRID_LONGITUDES_DEG_E)
+                    )
+                    axes_object.set_ylim(
+                        numpy.min(OUTER_GRID_LATITUDES_DEG_N),
+                        numpy.max(OUTER_GRID_LATITUDES_DEG_N)
+                    )
+
+                    title_string = 'GFS-forecast {0:s} at {1:d}-day lead'.format(
+                        target_field_names[j],
+                        gfs_forecast_target_lead_times_days[this_l]
+                    )
+                    axes_object.set_title(title_string)
+
+                    output_file_name = (
+                        '{0:s}/example{1:03d}_{2:s}_{3:03d}days.jpg'
+                    ).format(
+                        output_dir_name,
+                        example_index,
+                        target_field_names[j].replace('_', '-'),
+                        gfs_forecast_target_lead_times_days[this_l]
+                    )
+
+                    print('Saving figure to: "{0:s}"...'.format(
+                        output_file_name
+                    ))
+                    figure_object.savefig(
+                        output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+                        pad_inches=0, bbox_inches='tight'
+                    )
+                    pyplot.close(figure_object)
+
+            for j in range(num_target_fields):
+                figure_object, axes_object = pyplot.subplots(
+                    1, 1,
+                    figsize=(FIGURE_WIDTH_INCHES, FIGURE_HEIGHT_INCHES)
                 )
-            )
-            fwi_plotting.plot_field(
-                data_matrix=target_matrix_with_weights[i, ..., 0],
-                grid_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
-                grid_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
-                colour_map_object=colour_map_object,
-                colour_norm_object=colour_norm_object,
-                axes_object=axes_object,
-                plot_colour_bar=True
-            )
 
-            plotting_utils.plot_borders(
-                border_latitudes_deg_n=border_latitudes_deg_n,
-                border_longitudes_deg_e=border_longitudes_deg_e,
-                axes_object=axes_object,
-                line_colour=numpy.full(3, 0.)
-            )
-            plotting_utils.plot_grid_lines(
-                plot_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
-                plot_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
-                axes_object=axes_object,
-                meridian_spacing_deg=20.,
-                parallel_spacing_deg=10.
-            )
+                colour_map_object, colour_norm_object = (
+                    fwi_plotting.field_to_colour_scheme(
+                        target_field_names[j]
+                    )
+                )
+                fwi_plotting.plot_field(
+                    data_matrix=target_matrix_with_weights[i, ..., j],
+                    grid_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                    grid_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                    colour_map_object=colour_map_object,
+                    colour_norm_object=colour_norm_object,
+                    axes_object=axes_object,
+                    plot_colour_bar=True
+                )
 
-            axes_object.set_xlim(
-                numpy.min(OUTER_GRID_LONGITUDES_DEG_E),
-                numpy.max(OUTER_GRID_LONGITUDES_DEG_E)
-            )
-            axes_object.set_ylim(
-                numpy.min(OUTER_GRID_LATITUDES_DEG_N),
-                numpy.max(OUTER_GRID_LATITUDES_DEG_N)
-            )
+                plotting_utils.plot_borders(
+                    border_latitudes_deg_n=border_latitudes_deg_n,
+                    border_longitudes_deg_e=border_longitudes_deg_e,
+                    axes_object=axes_object,
+                    line_colour=numpy.full(3, 0.)
+                )
+                plotting_utils.plot_grid_lines(
+                    plot_latitudes_deg_n=OUTER_GRID_LATITUDES_DEG_N,
+                    plot_longitudes_deg_e=OUTER_GRID_LONGITUDES_DEG_E,
+                    axes_object=axes_object,
+                    meridian_spacing_deg=20.,
+                    parallel_spacing_deg=10.
+                )
 
-            axes_object.set_title('Target field (FFMC)')
+                axes_object.set_xlim(
+                    numpy.min(OUTER_GRID_LONGITUDES_DEG_E),
+                    numpy.max(OUTER_GRID_LONGITUDES_DEG_E)
+                )
+                axes_object.set_ylim(
+                    numpy.min(OUTER_GRID_LATITUDES_DEG_N),
+                    numpy.max(OUTER_GRID_LATITUDES_DEG_N)
+                )
 
-            output_file_name = '{0:s}/example{1:03d}_targets.jpg'.format(
-                output_dir_name, example_index
-            )
-            print('Saving figure to: "{0:s}"...'.format(
-                output_file_name
-            ))
-            figure_object.savefig(
-                output_file_name, dpi=FIGURE_RESOLUTION_DPI,
-                pad_inches=0, bbox_inches='tight'
-            )
-            pyplot.close(figure_object)
+                axes_object.set_title('Target field ({0:s})'.format(
+                    target_field_names[j]
+                ))
+
+                output_file_name = '{0:s}/example{1:03d}_{2:s}_target.jpg'.format(
+                    output_dir_name,
+                    example_index,
+                    target_field_names[j].replace('_', '-')
+                )
+                print('Saving figure to: "{0:s}"...'.format(
+                    output_file_name
+                ))
+                figure_object.savefig(
+                    output_file_name, dpi=FIGURE_RESOLUTION_DPI,
+                    pad_inches=0, bbox_inches='tight'
+                )
+                pyplot.close(figure_object)
 
             figure_object, axes_object = pyplot.subplots(
                 1, 1,
@@ -484,8 +655,8 @@ if __name__ == '__main__':
         era5_constant_predictor_field_names=getattr(
             INPUT_ARG_OBJECT, training_args.ERA5_CONSTANT_PREDICTORS_ARG_NAME
         ),
-        target_field_name=getattr(
-            INPUT_ARG_OBJECT, training_args.TARGET_FIELD_ARG_NAME
+        target_field_names=getattr(
+            INPUT_ARG_OBJECT, training_args.TARGET_FIELDS_ARG_NAME
         ),
         target_lead_time_days=getattr(
             INPUT_ARG_OBJECT, training_args.TARGET_LEAD_TIME_ARG_NAME
@@ -494,9 +665,12 @@ if __name__ == '__main__':
             getattr(INPUT_ARG_OBJECT, training_args.TARGET_LAG_TIMES_ARG_NAME),
             dtype=int
         ),
-        target_cutoffs_for_classifn=numpy.array(
-            getattr(INPUT_ARG_OBJECT, training_args.TARGET_CUTOFFS_ARG_NAME),
-            dtype=float
+        gfs_forecast_target_lead_times_days=numpy.array(
+            getattr(
+                INPUT_ARG_OBJECT,
+                training_args.GFS_FCST_TARGET_LEAD_TIMES_ARG_NAME
+            ),
+            dtype=int
         ),
         target_normalization_file_name=getattr(
             INPUT_ARG_OBJECT, training_args.TARGET_NORM_FILE_ARG_NAME
@@ -513,6 +687,10 @@ if __name__ == '__main__':
         target_dir_name_for_training=getattr(
             INPUT_ARG_OBJECT, training_args.TARGET_TRAINING_DIR_ARG_NAME
         ),
+        gfs_forecast_target_dir_name_for_training=getattr(
+            INPUT_ARG_OBJECT,
+            training_args.GFS_FCST_TARGET_TRAINING_DIR_ARG_NAME
+        ),
         init_date_limit_strings_for_training=getattr(
             INPUT_ARG_OBJECT, training_args.TRAINING_DATE_LIMITS_ARG_NAME
         ),
@@ -521,6 +699,10 @@ if __name__ == '__main__':
         ),
         target_dir_name_for_validation=getattr(
             INPUT_ARG_OBJECT, training_args.TARGET_VALIDATION_DIR_ARG_NAME
+        ),
+        gfs_forecast_target_dir_name_for_validation=getattr(
+            INPUT_ARG_OBJECT,
+            training_args.GFS_FCST_TARGET_VALIDATION_DIR_ARG_NAME
         ),
         init_date_limit_strings_for_validation=getattr(
             INPUT_ARG_OBJECT, training_args.VALIDATION_DATE_LIMITS_ARG_NAME
---
+++ ml_for_wildfire_wpo/slurm/experiment04_ffmc_regression/plot_generator_outputs_norm.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+template_file_name=$1
+output_dir_name=$2
+gfs_lead_time_string=$3
+
+gfs_lead_time_string=$(echo $gfs_lead_time_string | tr "-" " ")
+
+CODE_DIR_NAME="/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_standalone/ml_for_wildfire_wpo"
+
+GFS_DIR_NAME="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/gfs_data/processed/normalized_params_from_2019-2020"
+TARGET_DIR_NAME="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/canadian_fwi"
+GFS_FORECAST_TARGET_DIR_NAME="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/gfs_data/processed_fwi_forecasts"
+TARGET_NORM_FILE_NAME="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/canadian_fwi/z_score_params_2019-2020.nc"
+
+python3 -u "${CODE_DIR_NAME}/plot_generator_outputs.py" \
+--input_template_file_name="${template_file_name}" \
+--output_model_dir_name="${output_dir_name}" \
+--inner_latitude_limits_deg_n 17 73 \
+--inner_longitude_limits_deg_e 171 -65 \
+--outer_latitude_buffer_deg=5 \
+--outer_longitude_buffer_deg=5 \
+--gfs_predictor_field_names "temperature_kelvins" "specific_humidity_kg_kg01" "geopotential_height_m_asl" "u_wind_m_s01" "v_wind_m_s01" "temperature_2m_agl_kelvins" "specific_humidity_2m_agl_kg_kg01" "u_wind_10m_agl_m_s01" "v_wind_10m_agl_m_s01" "accumulated_precip_metres" "volumetric_soil_moisture_fraction_0to10cm" "snow_depth_metres" \
+--gfs_pressure_levels_mb 500 700 900 \
+--gfs_predictor_lead_times_hours ${gfs_lead_time_string} \
+--gfs_normalization_file_name="" \
+--era5_constant_file_name="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/era5_constants.nc" \
+--target_field_names "fine_fuel_moisture_code" \
+--target_lead_time_days=2 \
+--target_lag_times_days 1 2 3 \
+--gfs_forecast_target_lead_times_days 1 2 3 \
+--target_normalization_file_name="${TARGET_NORM_FILE_NAME}" \
+--num_examples_per_batch=8 \
+--sentinel_value=-10 \
+--gfs_dir_name_for_training="${GFS_DIR_NAME}" \
+--target_dir_name_for_training="${TARGET_DIR_NAME}" \
+--gfs_forecast_target_dir_name_for_training="${GFS_FORECAST_TARGET_DIR_NAME}" \
+--gfs_init_date_limit_strings_for_training "20190101" "20201210" \
+--gfs_dir_name_for_validation="${GFS_DIR_NAME}" \
+--target_dir_name_for_validation="${TARGET_DIR_NAME}" \
+--gfs_forecast_target_dir_name_for_validation="${GFS_FORECAST_TARGET_DIR_NAME}" \
+--gfs_init_date_limit_strings_for_validation "20210101" "20211210" \
+--num_epochs=1000 \
+--num_training_batches_per_epoch=16 \
+--num_validation_batches_per_epoch=8
