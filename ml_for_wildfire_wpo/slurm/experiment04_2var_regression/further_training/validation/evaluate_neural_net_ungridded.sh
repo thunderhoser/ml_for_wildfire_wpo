9e41bc6c741a4aa8aca406585f707dce88c6fc7b
thunderhoser <lagerqui@ualberta.ca>
2024-02-16 17:48:42 -0700

    FOO

--- ml_for_wildfire_wpo/slurm/experiment04_2var_regression/further_training/validation/evaluate_neural_net_ungridded.sh
+++ ml_for_wildfire_wpo/slurm/experiment04_2var_regression/further_training/validation/evaluate_neural_net_ungridded.sh
@@ -9,8 +9,8 @@ python3 -u "${CODE_DIR_NAME}/evaluate_model.py" \
 --init_date_limit_strings "20210101" "20211210" \
 --num_bootstrap_reps=1 \
 --target_field_names "fine_fuel_moisture_code" "buildup_index" \
---num_relia_bins_by_target 102 \
---min_relia_bin_edge_by_target 0 \
---max_relia_bin_edge_by_target 102 \
+--num_relia_bins_by_target 102 250 \
+--min_relia_bin_edge_by_target 0 0 \
+--max_relia_bin_edge_by_target 102 2500 \
 --per_grid_cell=0 \
 --output_file_name="${model_dir_name}/validation/ungridded_evaluation.nc"
