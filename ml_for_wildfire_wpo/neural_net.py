3fd2e8fbefb9d7904fd4d41867ff34d112b65ffd
thunderhoser <lagerqui@ualberta.ca>
2024-02-22 12:52:16 -0700

    FOO

--- ml_for_wildfire_wpo/neural_net.py
+++ ml_for_wildfire_wpo/neural_net.py
@@ -10,7 +10,7 @@ import numpy
 import keras
 import tensorflow
 
-tensorflow.compat.v1.disable_eager_execution()
+# tensorflow.compat.v1.disable_eager_execution()
 
 import tensorflow.keras as tf_keras
 
