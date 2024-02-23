Fixed stupid bug in normalization.py.

ml_for_wildfire_wpo/utils/normalization.py
@@ -157,8 +157,6 @@ def _quantile_normalize_1var(data_values, reference_values_1d):
 
     # TODO(thunderhoser): Still need unit test.
 
-    assert numpy.all(numpy.isfinite(reference_values_1d))
-
     data_values_1d = numpy.ravel(data_values)
     data_values_1d[numpy.invert(numpy.isfinite(data_values_1d))] = numpy.nan
 
@@ -168,6 +166,10 @@ def _quantile_normalize_1var(data_values, reference_values_1d):
 
     if len(real_indices) == 0:
         return data_values
+
+    if numpy.all(numpy.isnan(reference_values_1d)):
+        data_values_1d[real_indices] = 0.
+        return numpy.reshape(data_values_1d, data_values.shape)
 
     # The code below might fail due to non-unique values in reference_values_1d.
 
@@ -180,13 +182,17 @@ def _quantile_normalize_1var(data_values, reference_values_1d):
     # )
     # data_values_1d[real_indices] = interp_object(data_values_1d[real_indices])
 
+    real_reference_values_1d = reference_values_1d[
+        numpy.invert(numpy.isnan(reference_values_1d))
+    ]
+
     search_indices = numpy.searchsorted(
-        numpy.sort(reference_values_1d),
+        numpy.sort(real_reference_values_1d),
         data_values_1d[real_indices],
         side='left'
     ).astype(float)
 
-    num_reference_vals = len(reference_values_1d)
+    num_reference_vals = len(real_reference_values_1d)
     data_values_1d[real_indices] = search_indices / (num_reference_vals - 1)
 
     data_values_1d[real_indices] = numpy.minimum(
@@ -213,8 +219,6 @@ def _quantile_denormalize_1var(data_values, reference_values_1d):
 
     # TODO(thunderhoser): Still need unit test.
 
-    assert numpy.all(numpy.isfinite(reference_values_1d))
-
     data_values_1d = numpy.ravel(data_values)
     real_indices = numpy.where(
         numpy.invert(numpy.isnan(data_values_1d))
@@ -222,15 +226,20 @@ def _quantile_denormalize_1var(data_values, reference_values_1d):
 
     if len(real_indices) == 0:
         return data_values
+    if numpy.all(numpy.isnan(reference_values_1d)):
+        return data_values
 
     data_values_1d[real_indices] = scipy.stats.norm.cdf(
         data_values_1d[real_indices], loc=0., scale=1.
     )
+    real_reference_values_1d = reference_values_1d[
+        numpy.invert(numpy.isnan(reference_values_1d))
+    ]
 
     # Linear produces biased estimates (range of 0...0.1 in my test), while
     # lower produces unbiased estimates (range of -0.1...+0.1 in my test).
     data_values_1d[real_indices] = numpy.percentile(
-        numpy.ravel(reference_values_1d),
+        numpy.ravel(real_reference_values_1d),
         100 * data_values_1d[real_indices],
         interpolation='linear'
         # interpolation='lower'