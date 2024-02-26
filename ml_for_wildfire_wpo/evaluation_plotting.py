eec7b69e443c154dc467dcebca50e42c75b4780b
thunderhoser <lagerqui@ualberta.ca>
2024-02-26 15:05:53 -0700

    FOO

--- ml_for_wildfire_wpo/evaluation_plotting.py
+++ ml_for_wildfire_wpo/evaluation_plotting.py
@@ -302,12 +302,15 @@ def plot_inset_histogram(
         for this_tick_object in inset_axes_object.yaxis.get_major_ticks():
             this_tick_object.label.set_fontsize(HISTOGRAM_FONT_SIZE)
     except:
-        for this_tick_object in inset_axes_object.xaxis.get_major_ticks():
-            this_tick_object._label.set_fontsize(HISTOGRAM_FONT_SIZE)
-            this_tick_object._label.set_rotation('vertical')
+        try:
+            for this_tick_object in inset_axes_object.xaxis.get_major_ticks():
+                this_tick_object.set_fontsize(HISTOGRAM_FONT_SIZE)
+                this_tick_object.set_rotation('vertical')
 
-        for this_tick_object in inset_axes_object.yaxis.get_major_ticks():
-            this_tick_object._label.set_fontsize(HISTOGRAM_FONT_SIZE)
+            for this_tick_object in inset_axes_object.yaxis.get_major_ticks():
+                this_tick_object.set_fontsize(HISTOGRAM_FONT_SIZE)
+        except:
+            pass
 
     inset_axes_object.set_title(
         'Prediction frequency' if has_predictions else 'Observation frequency',
