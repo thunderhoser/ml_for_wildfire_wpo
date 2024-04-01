09fa1e29298a8040b1728cc51b17808a3451a54f
thunderhoser <lagerqui@ualberta.ca>
2024-04-01 10:49:29 -0600

    FOO

--- ml_for_wildfire_wpo/custom_losses.py
+++ ml_for_wildfire_wpo/custom_losses.py
@@ -511,8 +511,23 @@ def dual_weighted_crps_constrained_dsr(
         #     elems=(relevant_prediction_tensor, censored_relevant_prediction_tensor)
         # )
 
-        mean_prediction_diff_tensor = K.map_fn(
-            fn=lambda p: K.mean(
+        # mean_prediction_diff_tensor = K.map_fn(
+        #     fn=lambda p: K.mean(
+        #         K.maximum(
+        #             K.abs(K.expand_dims(p, axis=-1)),
+        #             K.abs(K.expand_dims(p, axis=-2))
+        #         ) *
+        #         K.abs(
+        #             K.expand_dims(p, axis=-1) -
+        #             K.expand_dims(p, axis=-2)
+        #         ),
+        #         axis=(-2, -1)
+        #     ),
+        #     elems=relevant_prediction_tensor
+        # )
+
+        mean_prediction_diff_tensor = K.stack([
+            K.mean(
                 K.maximum(
                     K.abs(K.expand_dims(p, axis=-1)),
                     K.abs(K.expand_dims(p, axis=-2))
@@ -522,9 +537,9 @@ def dual_weighted_crps_constrained_dsr(
                     K.expand_dims(p, axis=-2)
                 ),
                 axis=(-2, -1)
-            ),
-            elems=relevant_prediction_tensor
-        )
+            )
+            for p in relevant_prediction_tensor
+        ], axis=0)
 
         print('First mean_prediction_diff_tensor.shape = {0:s}'.format(
             str(mean_prediction_diff_tensor.shape)
