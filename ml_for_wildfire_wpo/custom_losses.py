ac2074f3a18c9d048ec9f8fe17edbcd9760422e3
thunderhoser <lagerqui@ualberta.ca>
2024-03-31 19:42:32 -0600

    FOO

--- ml_for_wildfire_wpo/custom_losses.py
+++ ml_for_wildfire_wpo/custom_losses.py
@@ -502,6 +502,7 @@ def dual_weighted_crps_constrained_dsr(
             ),
             elems=relevant_prediction_tensor
         )
+        print(mean_prediction_diff_tensor.shape)
 
         mean_prediction_diff_tensor = K_ops.swapaxes(mean_prediction_diff_tensor, 0, 1)
         print(mean_prediction_diff_tensor.shape)
