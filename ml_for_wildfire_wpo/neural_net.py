0f1d53a643d09b2f100dd8c4c9b686dc10f61ab9
thunderhoser <lagerqui@ualberta.ca>
2024-05-02 14:15:58 -0600

    FOO

--- ml_for_wildfire_wpo/neural_net.py
+++ ml_for_wildfire_wpo/neural_net.py
@@ -1462,23 +1462,23 @@ def data_generator(option_dict):
         ]
 
         predictor_matrices = {}
-        if gfs_predictor_matrix_3d is not None:
-            predictor_matrices.update({
-                'gfs_3d_inputs': gfs_predictor_matrix_3d.astype('float32')
-            })
-        if gfs_predictor_matrix_2d is not None:
-            predictor_matrices.update({
-                'gfs_2d_inputs': gfs_predictor_matrix_2d.astype('float32')
-            })
-        if era5_constant_matrix is not None:
-            predictor_matrices.update({
-                'era5_inputs': era5_constant_matrix.astype('float32')
-            })
-        if laglead_target_predictor_matrix is not None:
-            predictor_matrices.update({
-                'lagged_target_inputs':
-                    laglead_target_predictor_matrix.astype('float32')
-            })
+        # if gfs_predictor_matrix_3d is not None:
+        #     predictor_matrices.update({
+        #         'gfs_3d_inputs': gfs_predictor_matrix_3d.astype('float32')
+        #     })
+        # if gfs_predictor_matrix_2d is not None:
+        #     predictor_matrices.update({
+        #         'gfs_2d_inputs': gfs_predictor_matrix_2d.astype('float32')
+        #     })
+        # if era5_constant_matrix is not None:
+        #     predictor_matrices.update({
+        #         'era5_inputs': era5_constant_matrix.astype('float32')
+        #     })
+        # if laglead_target_predictor_matrix is not None:
+        #     predictor_matrices.update({
+        #         'lagged_target_inputs':
+        #             laglead_target_predictor_matrix.astype('float32')
+        #     })
         if baseline_prediction_matrix is not None:
             predictor_matrices.update({
                 'predn_baseline_inputs':
