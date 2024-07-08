9074c8a49b8f5c3a06b2c715709b3fdbe4c98bc1
thunderhoser <lagerqui@ualberta.ca>
2024-07-08 13:24:31 -0600

    FOO

--- ml_for_wildfire_wpo/chiu_net_pp_architecture.py
+++ ml_for_wildfire_wpo/chiu_net_pp_architecture.py
@@ -1377,14 +1377,10 @@ def create_flexible_lead_time_model(option_dict, loss_function, metric_list):
         shape1, shape2 = input_shapes
         return (shape1[0], shape2[1], shape1[2], shape1[3], shape1[4])
 
-    print(layer_object_constants)
-
     this_layer_object = keras.layers.Lambda(
         lambda x: __repeat_tensor(x[0], x[1]), name='first_repeated_tensor',
         output_shape=dynamic_output_shape
     )([layer_object_constants, layer_object_gfs])
-
-    print(this_layer_object)
 
     # this_layer_object = keras.layers.Concatenate(
     #     axis=-4, name='const_add-gfs-times'
@@ -1398,12 +1394,10 @@ def create_flexible_lead_time_model(option_dict, loss_function, metric_list):
         [layer_object_gfs, this_layer_object]
     )
 
-    these_dims = (None,) + layer_object_constants.shape[2:]
-    print(these_dims)
     this_layer_object = keras.layers.Lambda(
         lambda x: __repeat_tensor(x[0], x[1]), name='second_repeated_tensor',
-        output_shape=these_dims
-    )([layer_object_constants, num_target_lag_times])
+        output_shape=dynamic_output_shape
+    )([layer_object_constants, input_layer_object_lagged_target])
 
     # this_layer_object = keras.layers.Concatenate(
     #     axis=-4, name='const_add-lag-times'
