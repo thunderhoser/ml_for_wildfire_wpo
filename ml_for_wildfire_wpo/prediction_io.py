24e9b8e028dab7f5fc1b7e852b8780983c4b07e3
thunderhoser <lagerqui@ualberta.ca>
2024-08-12 17:05:53 -0600

    FOO

--- ml_for_wildfire_wpo/prediction_io.py
+++ ml_for_wildfire_wpo/prediction_io.py
@@ -441,6 +441,9 @@ def subset_by_location(
 
     eval_weight_matrix[keep_location_matrix == False] = 0.
     good_rows, good_columns = numpy.where(keep_location_matrix)
+    good_rows = numpy.unique(good_rows)
+    good_columns = numpy.unique(good_columns)
+
     print(keep_location_matrix.shape)
     print(good_rows)
     print(good_columns)
