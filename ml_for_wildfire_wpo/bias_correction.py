06e77e1c4606ef03ea5bdd340ac8e69a46ab963a
thunderhoser <lagerqui@ualberta.ca>
2024-10-07 10:37:12 -0600

    FOO

--- ml_for_wildfire_wpo/bias_correction.py
+++ ml_for_wildfire_wpo/bias_correction.py
@@ -684,10 +684,6 @@ def train_model_suite_not_per_pixel(
             )[0]) for f in field_names
         ], dtype=int)
 
-        print(cluster_table_xarray)
-        print(field_names)
-        print(sort_indices)
-
         cluster_table_xarray = cluster_table_xarray.isel(
             {bias_clustering.FIELD_DIM: sort_indices}
         )
