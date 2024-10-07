2970d8acdd6b9e9040ed7f52707d191d776255ee
thunderhoser <lagerqui@ualberta.ca>
2024-10-07 13:40:00 -0600

    FOO

---
+++ ml_for_wildfire_wpo/test_hdf5.py
@@ -0,0 +1,15 @@
+"""USE ONCE AND DESTROY."""
+
+import h5py
+
+MODEL_FILE_NAME = '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml_for_wildfire_models/experiment17/training-strategy=20-30-daily_spectral-complexity=080_use-residual-blocks=0/model.weights.h5'
+
+# Open the file in read mode
+with h5py.File(MODEL_FILE_NAME, 'r') as f:
+    # List all groups and datasets
+    print(list(f.keys()))
+
+    # Access a specific dataset
+    dataset = f['dataset_name']
+    data = dataset[:]
+    print(data)
