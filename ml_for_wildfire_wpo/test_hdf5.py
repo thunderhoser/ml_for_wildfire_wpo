cdc3a58394a4cf695850672e27807d12b16799d0
thunderhoser <lagerqui@ualberta.ca>
2024-10-08 13:45:00 -0600

    FOO

--- ml_for_wildfire_wpo/test_hdf5.py
+++ ml_for_wildfire_wpo/test_hdf5.py
@@ -4,9 +4,8 @@ import h5py
 
 MODEL_FILE_NAME = '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml_for_wildfire_models/experiment17/training-strategy=20-30-daily_spectral-complexity=080_use-residual-blocks=0/model.weights.h5'
 
-# Open the file in read mode
 with h5py.File(MODEL_FILE_NAME, 'r') as f:
-    # List all groups and datasets in the file
-    def print_structure(name, obj):
-        print(f"{name}: {type(obj)}")
-    f.visititems(print_structure)
+    print("Keys in the weights file:", list(f.keys()))
+    # Optionally, inspect individual layers
+    for layer in f.keys():
+        print(f"Layer: {layer}, Weights: {list(f[layer].keys())}")
