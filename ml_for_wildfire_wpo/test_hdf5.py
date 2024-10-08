391c1f610992d6939c96dfb473b0be1d67eb0a37
thunderhoser <lagerqui@ualberta.ca>
2024-10-08 13:53:04 -0600

    FOO

--- ml_for_wildfire_wpo/test_hdf5.py
+++ ml_for_wildfire_wpo/test_hdf5.py
@@ -2,7 +2,7 @@
 
 import h5py
 
-MODEL_FILE_NAME = '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml_for_wildfire_models/experiment17/training-strategy=20-30-daily_spectral-complexity=080_use-residual-blocks=0/model.weights.h5'
+MODEL_FILE_NAME = '/scratch1/RDARCH/rda-ghpcs/Ryan.Lagerquist/ml_for_wildfire_models/experiment17/training-strategy=20-30-daily_spectral-complexity=080_use-residual-blocks=0/better_weight_file/model.weights.h5'
 
 with h5py.File(MODEL_FILE_NAME, 'r') as f:
     print("Keys in the weights file:", list(f.keys()))
