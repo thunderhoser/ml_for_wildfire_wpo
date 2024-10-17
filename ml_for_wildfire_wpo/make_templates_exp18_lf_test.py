19e648fceb3a2b3cbc7fc228a1ded67d9de20a14
thunderhoser <lagerqui@ualberta.ca>
2024-10-17 12:35:28 -0600

    FOO

--- ml_for_wildfire_wpo/make_templates_exp18_lf_test.py
+++ ml_for_wildfire_wpo/make_templates_exp18_lf_test.py
@@ -134,7 +134,6 @@ METRIC_FUNCTIONS = [
     custom_losses.dual_weighted_crpss(channel_weights=CHANNEL_WEIGHTS[:4], max_dual_weight_by_channel=MAX_DUAL_WEIGHTS[:4], dmc_index=1, dc_index=2, isi_index=3, function_name='dwcrpss_no_constraints'),
     custom_losses.dual_weighted_crpss_constrained_bui(channel_weights=CHANNEL_WEIGHTS[:5], max_dual_weight_by_channel=MAX_DUAL_WEIGHTS[:5], dmc_index=1, dc_index=2, isi_index=3, function_name='dwcrpss_constrained_bui'),
     custom_losses.dual_weighted_crpss_constrained_bui_fwi(channel_weights=CHANNEL_WEIGHTS[:6], max_dual_weight_by_channel=MAX_DUAL_WEIGHTS[:6], dmc_index=1, dc_index=2, isi_index=3, function_name='dwcrpss_constrained_bui_fwi'),
-    custom_losses.dual_weighted_crpss_constrained_bui_fwi_together(channel_weights=CHANNEL_WEIGHTS[:6], max_dual_weight_by_channel=MAX_DUAL_WEIGHTS[:6], dmc_index=1, dc_index=2, isi_index=3, function_name='dwcrpss_constrained_bui_fwi_together'),
     custom_losses.dual_weighted_crpss_all_constraints(channel_weights=CHANNEL_WEIGHTS, max_dual_weight_by_channel=MAX_DUAL_WEIGHTS, dmc_index=1, dc_index=2, isi_index=3, function_name='dwcrpss_all_constraints'),
 ]
 
@@ -226,7 +225,6 @@ METRIC_FUNCTION_STRINGS = [
     'custom_losses.dual_weighted_crpss(channel_weights=numpy.array([0.02562263, 0.00373885, 0.00008940, 0.60291427, 0.00251213, 0.08761268, 0.27751004])[:4], max_dual_weight_by_channel=numpy.array([96.4105, 303.9126, 1741.9033, 23.1660, 361.6984, 61.3669, 40.1856])[:4], dmc_index=1, dc_index=2, isi_index=3, function_name="dwcrpss_no_constraints")',
     'custom_losses.dual_weighted_crpss_constrained_bui(channel_weights=numpy.array([0.02562263, 0.00373885, 0.00008940, 0.60291427, 0.00251213, 0.08761268, 0.27751004])[:5], max_dual_weight_by_channel=numpy.array([96.4105, 303.9126, 1741.9033, 23.1660, 361.6984, 61.3669, 40.1856])[:5], dmc_index=1, dc_index=2, isi_index=3, function_name="dwcrpss_constrained_bui")',
     'custom_losses.dual_weighted_crpss_constrained_bui_fwi(channel_weights=numpy.array([0.02562263, 0.00373885, 0.00008940, 0.60291427, 0.00251213, 0.08761268, 0.27751004])[:6], max_dual_weight_by_channel=numpy.array([96.4105, 303.9126, 1741.9033, 23.1660, 361.6984, 61.3669, 40.1856])[:6], dmc_index=1, dc_index=2, isi_index=3, function_name="dwcrpss_constrained_bui_fwi")',
-    'custom_losses.dual_weighted_crpss_constrained_bui_fwi_together(channel_weights=numpy.array([0.02562263, 0.00373885, 0.00008940, 0.60291427, 0.00251213, 0.08761268, 0.27751004])[:6], max_dual_weight_by_channel=numpy.array([96.4105, 303.9126, 1741.9033, 23.1660, 361.6984, 61.3669, 40.1856])[:6], dmc_index=1, dc_index=2, isi_index=3, function_name="dwcrpss_constrained_bui_fwi_together")',
     'custom_losses.dual_weighted_crpss_all_constraints(channel_weights=numpy.array([0.02562263, 0.00373885, 0.00008940, 0.60291427, 0.00251213, 0.08761268, 0.27751004]), max_dual_weight_by_channel=numpy.array([96.4105, 303.9126, 1741.9033, 23.1660, 361.6984, 61.3669, 40.1856]), dmc_index=1, dc_index=2, isi_index=3, function_name="dwcrpss_all_constraints")',
 ]
 
