bed6e9584cfcfcb02de689b291084aee302ab890
thunderhoser <lagerqui@ualberta.ca>
2024-12-18 12:07:36 -0700

    FOO

--- ml_for_wildfire_wpo/slurm/experiment19_mountain_central_west/shapley_test/test_shapley_maps.sh
+++ ml_for_wildfire_wpo/slurm/experiment19_mountain_central_west/shapley_test/test_shapley_maps.sh
@@ -18,7 +18,7 @@ python3 -u "${CODE_DIR_NAME}/make_shapley_maps.py" \
 --baseline_init_date_strings "20200801" "20200802" "20200803" "20200804" "20200805" "20200806" "20200807" "20200808" "20200809" "20200810" \
 --new_init_date_strings "20210809" "20210810" \
 --input_region_mask_file_name="/scratch2/BMC/gsd-hpcs/Ryan.Lagerquist/ml_for_wildfire_wpo_project/smaller_colorado_mask.nc" \
---target_field_name="fire_weather_index" \
+--target_field_name="fine_fuel_moisture_code" \
 --use_inout_tensors_only=0 \
 --disable_tensorflow2=1 \
 --disable_eager_execution=1 \
