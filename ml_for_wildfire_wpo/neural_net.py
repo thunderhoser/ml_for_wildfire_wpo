52da048e7217c6380b3ddb92f0db1fadb1a165a9
thunderhoser <lagerqui@ualberta.ca>
2025-03-03 11:08:18 -0700

    FOO

--- ml_for_wildfire_wpo/neural_net.py
+++ ml_for_wildfire_wpo/neural_net.py
@@ -3287,22 +3287,13 @@ def create_data(
     option_dict[INIT_DATE_LIMITS_KEY] = [init_date_string] * 2
     option_dict[BATCH_SIZE_KEY] = 32
 
-    try:
-        all_model_leads_days = list(set(
-            this_key[1] for this_key in
-            option_dict[MODEL_LEAD_TO_FREQ_KEY].keys()
-        ))
-        all_model_leads_days = numpy.array(all_model_leads_days, dtype=int)
-    except:
-        all_model_leads_days = numpy.array(
-            list(option_dict[MODEL_LEAD_TO_FREQ_KEY].keys()),
-            dtype=int
-        )
-        all_model_leads_days = numpy.unique(all_model_leads_days)
-
-    dummy_frequencies = numpy.full(len(all_model_leads_days), 0.1)
+    # TODO(thunderhoser): This might cause error in `_check_generator_args`
+    # sometimes.
+    dummy_model_leads_days = numpy.linspace(1, 14, num=14, dtype=int)
+    dummy_frequencies = numpy.full(len(dummy_model_leads_days), 1.)
+    dummy_frequencies = dummy_frequencies / numpy.sum(dummy_frequencies)
     option_dict[MODEL_LEAD_TO_FREQ_KEY] = dict(
-        zip(all_model_leads_days, dummy_frequencies)
+        zip(dummy_model_leads_days, dummy_frequencies)
     )
 
     option_dict = _check_generator_args(option_dict)
